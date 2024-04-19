4c41d169e6425ad3d031f0c3f653fd5a979073e4
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-04-19 21:58:13 +0100

    Trending videos section

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,5 +1,6 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
+import { Video, ResizeMode } from "expo-av";
 
 import { icons } from "../constants";
 
@@ -36,7 +37,18 @@ const VideoCard = ({ title, creator, avatar, thumbnail, video }) => {
         </View>
       </View>
       {play ? (
-        <Text>Playing</Text>
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           activeOpacity={0.7}
