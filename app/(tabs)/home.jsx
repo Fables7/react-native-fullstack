16d69b910138474a6ec174729804c84335af5889
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-04-19 23:00:40 +0100

    Profile screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  FlatList,
-  Image,
-  RefreshControl,
-  Text,
-  View
-} from "react-native";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import EmptyState from "../../components/EmptyState";
@@ -15,8 +9,10 @@ import VideoCard from "../../components/VideoCard";
 import { images } from "../../constants";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -46,10 +42,10 @@ const Home = () => {
             <View className=" justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as FormField } from "./FormField";
 export { default as SearchInput } from "./SearchInput";
 export { default as Trending } from "./Trending";
 export { default as VideoCard } from "./VideoCard";
+export { default as InfoBox } from "./InfoBox";
